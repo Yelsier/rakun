125a162829aceb02a5ae7a50713dccdef6668e32
yelsier <clarosyago@gmail.com>
2026-05-06 22:39:07 +0200

    feat: update version to 1.0.7 for @rakun-kit/core and 1.0.9 for @rakun-kit/next, add HelloWorld content type

---
+++ core/src/internal-content-types/HelloWorld.ts
@@ -0,0 +1,16 @@
+import ContentType from "../lib/ContentType";
+import { Fields } from "../lib/fields";
+import type { DataFront, DataInput, DBOutput } from "../lib/types";
+
+export const HelloWorld = new ContentType({
+  name: "HelloWorld",
+  fields: {
+    text: Fields.string().required(),
+  },
+  uniques: [["text"]],
+}).hideFromManager();
+
+export type HelloWorld = typeof HelloWorld;
+export type HelloWorldSchema = DataFront<HelloWorld>;
+export type HelloWorldManager = DBOutput<HelloWorld>;
+export type HelloWorldInput = DataInput<HelloWorld>;
--- core/src/internal-content-types/Page.ts
+++ core/src/internal-content-types/Page.ts
@@ -1,11 +1,18 @@
 import ContentType from "../lib/ContentType";
 import { Fields } from "../lib/fields";
+import { HelloWorld } from "./HelloWorld";
 
 export const Page = new ContentType({
   name: "Page",
   fields: {
     title: Fields.string().translatable().required(),
     slug: Fields.string().type("Slug").required().translatable(),
+    iterator: Fields.iterator([
+      {
+        contentType: HelloWorld,
+        type: "new",
+      },
+    ]).required(),
   },
   menu: {
     title: "Pages",
