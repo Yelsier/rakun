35c7837da03dbf16d6267d9e641f46e0ac92505c
John <z5512345z@gmail.com>
2026-07-29 12:29:31 +0200

    feat: hot reload on content model

--- preview-next/app/api/[[...rakun]]/route.ts
+++ preview-next/app/api/[[...rakun]]/route.ts
@@ -4,5 +4,6 @@ import { createPreviewBootstrap } from '../../../server/bootstrap'
 
 export const dynamic = 'force-dynamic'
 
-const bootstrap = createPreviewBootstrap()
-export const { GET, POST, PUT } = rakunNext({ bootstrap })
+export const { GET, POST, PUT } = rakunNext({
+  bootstrap: () => createPreviewBootstrap(),
+})
